36274607f409f9be7a63b76c5e8f52773158bfff
developrsteel <developrsteel@gmail.com>
2014-06-07 04:52:45 +0200

    Show corresponding list on drawer item click.

--- AlbumSample/src/main/java/com/sample/album/common/ItemsList.java
+++ AlbumSample/src/main/java/com/sample/album/common/ItemsList.java
@@ -2,10 +2,6 @@ package com.sample.album.common;
 
 import android.app.ListFragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import java.util.ArrayList;
 
 public class ItemsList extends ListFragment {
@@ -13,16 +9,39 @@ public class ItemsList extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int position = getArguments().getInt("position");
+
+        String title;
+        String color;
+        switch(position){
+            case 1:
+                title = "Songs";
+                color = "75CC00";
+                break;
+            case 2:
+                title = "Artists";
+                color = "E4DB0F";
+                break;
+            default:
+                title = "Albums";
+                color = "C50000";
+                break;
+        }
+
         // Generate a list of 1000 dummy items
         ArrayList<Item> dummies = new ArrayList<Item>();
         for (int i = 0; i < 1000; i++){
             Item item = new Item();
-            item.setMainHeader("Album " + i);
+            item.setMainHeader(title + " " + i);
             item.setSecondaryHeader("Gender " + i);
+            item.setIconUrl("http://placehold.it/100x100/" + color + "/ffffff.png&text=" + i);
             dummies.add(item);
         }
 
-        ItemsAdapter adapter = new ItemsAdapter(dummies, getActivity());
-        setListAdapter(adapter);
+        ItemsAdapter itemsAdapter = new ItemsAdapter(dummies, getActivity());
+        itemsAdapter.setGrid(false);
+        setListAdapter(itemsAdapter);
     }
+
 }
